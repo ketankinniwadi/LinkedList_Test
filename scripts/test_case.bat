d3e2df4515bd4b6b1e5589992ea7d626039ecf0f
Ketan Kinniwadi <ketank@prescient.in>
2025-04-29 14:24:36 +0530

    six

--- scripts/test_case.bat
+++ scripts/test_case.bat
@@ -6,7 +6,7 @@ if not exist build mkdir build
 if not exist reports mkdir reports
 
 :: Compile Java classes
-javac -cp .;libs\junit-platform-console-standalone-1.10.2.jar -d build src\test\java\LinkedListTest
+javac -cp .;libs\junit-platform-console-standalone-1.10.2.jar -d build src\test\java\LinkedListTest.java
 
 :: Run Tests
 java -jar libs\junit-platform-console-standalone-1.10.2.jar --class-path build --scan-class-path > reports\test_report.txt
