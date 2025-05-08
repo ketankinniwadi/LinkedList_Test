a64abf2cfebdcf37dd624fd659cf4c3d15ccb6fd
Ketan Kinniwadi <ketank@prescient.in>
2025-05-08 12:00:00 +0530

    9th commit

--- scripts/zip_build.bat
+++ scripts/zip_build.bat
@@ -1,3 +1,5 @@
 echo --- Zipping Build Folder ---
+
 if not exist artifacts mkdir artifacts
-powershell Compress-Archive -Path build,logs,reports -DestinationPath artifacts\build_%date:~10,4%%date:~4,2%%date:~7,2%.zip
+
+powershell Compress-Archive -Path build,logs,reports -DestinationPath artifacts\build_%date:~10,4%%date:~4,2%%date:~7,2%.zip -Force
